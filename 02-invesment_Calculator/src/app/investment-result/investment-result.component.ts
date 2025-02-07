446f5f1cefc45e692f052373b1b8c00ce1772d36
jinalparikh25 <theparikh303@gmail.com>
2025-02-07 13:07:00 +0100

    02-investment_Calculator -with blue print for header,user-input and investment-result component

---
+++ 02-invesment_Calculator/src/app/investment-result/investment-result.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-result',
+  standalone: true,
+  imports: [],
+  templateUrl: './investment-result.component.html',
+  styleUrl: './investment-result.component.css'
+})
+export class InvestmentResultComponent {
+
+  // function calculateInvestmentResults() {
+  //   const annualData = [];
+  //   let investmentValue = initialInvestment;
+  
+  //   for (let i = 0; i < duration; i++) {
+  //     const year = i + 1;
+  //     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+  //     investmentValue += interestEarnedInYear + annualInvestment;
+  //     const totalInterest =
+  //       investmentValue - annualInvestment * year - initialInvestment;
+  //     annualData.push({
+  //       year: year,
+  //       interest: interestEarnedInYear,
+  //       valueEndOfYear: investmentValue,
+  //       annualInvestment: annualInvestment,
+  //       totalInterest: totalInterest,
+  //       totalAmountInvested: initialInvestment + annualInvestment * year,
+  //     });
+  //   }
+  
+  //   return annualData;
+  // }
+  
+}
