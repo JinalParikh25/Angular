da89f6a11cda7292e2bf76087e524d8323ece9fe
jinalparikh25 <theparikh303@gmail.com>
2025-02-14 14:14:43 +0100

    finishing 3-cmp-deep-drive and modified Readme file

--- 03-cmp-deep-drive/src/app/dashboard/support-tickets/support-tickets.component.ts
+++ 03-cmp-deep-drive/src/app/dashboard/support-tickets/support-tickets.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+import { newTicket } from './new-ticket/new-ticket.model';
+import { ticketModel } from './support-tickets.model';
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-support-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent,TicketComponent],
   templateUrl: './support-tickets.component.html',
   styleUrl: './support-tickets.component.css'
 })
 export class SupportTicketsComponent {
 
+  tickets : ticketModel[] = [];
+
+  onFormSubmit(newTicket : newTicket){
+    this.tickets.push({
+      id:Math.random().toString(),
+      title : newTicket.title,
+      request : newTicket.request,
+      status : 'open'
+    });
+  }
 }
